a7f74f0e4aa77b90668176bcbecf7798d03e58b1
glayyiyi <glay.guo@gmail.com>
2014-03-09 20:38:55 +0800

    u

--- 1/wp-content/plugins/wptouch/themes/foundation/default/native-template.php
+++ 1/wp-content/plugins/wptouch/themes/foundation/default/native-template.php
@@ -12,12 +12,6 @@
 			<?php wptouch_the_post(); ?>
 
 			<div class="<?php wptouch_post_classes(); ?>">
-				<div class="post-head-area">
-					<?php // if ( has_post_thumbnail() ) the_post_thumbnail(); ?>  
-					<h2 class="post-title heading-font"><?php wptouch_the_title(); ?></h2>
-					<span class="post-date"><?php wptouch_the_time(); ?> &bull;</span>
-					<span class="post-author"><?php _e( 'By', 'wptouch-pro' ); ?> <?php the_author(); ?></span>
-				</div>
 				<?php wptouch_the_content(); ?>
 			</div>
 
