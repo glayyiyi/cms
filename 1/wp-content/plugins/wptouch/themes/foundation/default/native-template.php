0652eae815900e09e5e3a26d831f814419bbaed4
glayyiyi <glay.guo@gmail.com>
2014-03-09 20:54:45 +0800

    u

--- 1/wp-content/plugins/wptouch/themes/foundation/default/native-template.php
+++ 1/wp-content/plugins/wptouch/themes/foundation/default/native-template.php
@@ -9,9 +9,15 @@
 <div id="content">
 		<?php while ( wptouch_have_posts() ) { ?>
 		
-			<?php wptouch_the_post(); ?>
+			<?php 
+			do_action( 'wptouch_pre_head' );
+			wptouch_the_post(); ?>
 
 			<div class="<?php wptouch_post_classes(); ?>">
+				<div class="post-head-area">
+					<?php // if ( has_post_thumbnail() ) the_post_thumbnail(); ?>  
+					<h2 class="post-title heading-font"><?php wptouch_the_title(); ?></h2>
+				</div>
 				<?php wptouch_the_content(); ?>
 			</div>
 
