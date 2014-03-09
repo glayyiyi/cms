e7f41dd455f65852cb75a27e2f576893bd98fffb
glayyiyi <glay.guo@gmail.com>
2014-03-09 21:31:05 +0800

    u

--- 1/wp-content/plugins/wptouch/themes/foundation/default/native-template.php
+++ 1/wp-content/plugins/wptouch/themes/foundation/default/native-template.php
@@ -14,7 +14,7 @@ wptouch_the_post();
 ?>
 	
 <body class="page page-id-2810 page-template page-template-template-fullwidth-php logged-in css-videos tablet landscape ios dark-header light-body dark-post-head circles">
-<div class="page-wrapper">
+
 <div id="content">
 <div class="post section post-2810 post-name-promote-introduction post-author-1 not-single page no-thumbnail show-thumbs">
 	<div class="post-page-head-area bauhaus">
@@ -24,8 +24,7 @@ wptouch_the_post();
 	<?php wptouch_the_content() ; ?>
 	</div>
 </div>			
-</div>
-</div>
+
 </body>
 
 
