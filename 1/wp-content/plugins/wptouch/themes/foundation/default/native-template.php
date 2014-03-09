e16b8646a17bc75ca6d1576b9edb6236378e52d2
glayyiyi <glay.guo@gmail.com>
2014-03-09 19:40:41 +0800

    u

---
+++ 1/wp-content/plugins/wptouch/themes/foundation/default/native-template.php
@@ -0,0 +1,15 @@
+<?php
+/*
+	Mobile Template: Native Page
+*/
+?>
+
+
+<?php if ( have_posts() ) { ?>
+<div id="content">
+	<div class="post-content">
+		<?php wptouch_the_content(); ?>
+	</div>
+</div>
+<?php } ?>
+
