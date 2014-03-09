a8a20f4e150d59e1dd5187f8b550a75126903112
root <root@AY140211172914868a58Z.(none)>
2014-03-09 20:06:17 +0800

    add html

---
+++ 1/html/wp-gz-sign-in.php
@@ -0,0 +1,19 @@
+
+<html lang="zh-CN" class="boxed ">
+<head>
+
+<meta charset="UTF-8" />
+
+<title> 果子助手</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" type="text/css" href="http://www.appcn100.com/cms/iagent/wp-content/themes/mystile/style.css" media="screen" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<?php
+require( dirname(__FILE__) . '/../wp-load.php' );
+
+
+include '../wp-content/plugins/wp-gz/wp-gz-sign-in.php';
+
+?>
+</html>
