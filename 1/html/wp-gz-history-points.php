ea325e3a496c2051e81bdf7748011f5ed203e556
root <root@AY140211172914868a58Z.(none)>
2014-03-09 20:37:41 +0800

    add history-points

---
+++ 1/html/wp-gz-history-points.php
@@ -0,0 +1,30 @@
+
+<html lang="zh-CN" class="boxed ">
+<head>
+
+<meta charset="UTF-8" />
+
+<title> 果子助手</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" type="text/css" href="http://www.appcn100.com/cms/iagent/wp-content/themes/mystile/style.css" media="screen" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<?php
+require( dirname(__FILE__) . '/../wp-load.php' );
+?>
+
+
+<?php
+// if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+?>
+
+<?php wc_print_notices(); ?>
+
+<form class="login" method="post">
+    <div class="connerdiv">
+        <?php echo do_shortcode('[mycred_history user_id="'.$_GET['uid'].'" number="10"]'); ?>
+    </div>
+</form>
+<script type="text/javascript">
+
+</script>
