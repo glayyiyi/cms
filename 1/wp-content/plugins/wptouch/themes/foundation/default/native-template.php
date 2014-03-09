8e7e66c36984af864e83c0fe27b14de2e52720ca
glayyiyi <glay.guo@gmail.com>
2014-03-09 21:24:49 +0800

    u

--- 1/wp-content/plugins/wptouch/themes/foundation/default/native-template.php
+++ 1/wp-content/plugins/wptouch/themes/foundation/default/native-template.php
@@ -3,12 +3,22 @@
 	Mobile Template: Native Page
 */
 ?>
+<header>
+<link rel='stylesheet' id='foundation_font_awesome_css-css'  href='http://www.appcn100.com/cms/iagent/wp-content/plugins/wptouch/themes/foundation/modules/font-awesome/font-awesome.min.css?ver=2.0.4' type='text/css' media='screen' />
+<link rel='stylesheet' id='wptouch-parent-theme-css-css'  href='http://www.appcn100.com/cms/iagent/wp-content/plugins/wptouch/themes/foundation/default/style.css?ver=3.1.8' type='text/css' media='all' />
+<style type='text/css'>
+.page-wrapper { background-color: #f9f9f8; }
+a { color: #2d353f; }
+body, header, .wptouch-menu, #search-dropper, .date-circle { background-color: #2d353f; }
+a, #slider a p:after { color: #35c4ff; }
+.dots li.active, #switch .active { background-color: #35c4ff; }
+.bauhaus, .wptouch-login-wrap, form#commentform button#submit { background-color: #84ac50; }
 
-	
+</style>
+<link rel='stylesheet' id='wptouch-theme-css-css'  href='http://www.appcn100.com/cms/iagent/wp-content/plugins/wptouch/themes/bauhaus/default/style.css?ver=3.1.8' type='text/css' media='all' />
+</header>
 <?php 
-do_action( 'wptouch_pre_head' );
-//wp_head();
-do_action( 'wptouch_post_head' );
+
 wptouch_the_post(); 
 ?>
 	
