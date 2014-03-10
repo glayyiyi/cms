b4f7afb196440374dbb9b8131de82a8518724522
MOON <675448353@qq.com>
2014-03-10 10:41:35 +0800

    排行样式的修改及汉化

--- 1/wp-content/plugins/wp-gz/wp-gz-rank.php
+++ 1/wp-content/plugins/wp-gz/wp-gz-rank.php
@@ -4,12 +4,16 @@
 
 <?php wc_print_notices(); ?>
 
-<form class="login" method="post">
-    <div class="connerdiv">
-        <?php echo do_shortcode('[mycred_my_ranking user_id="'.$_GET['uid'].'"]'); ?>
-        <?php echo do_shortcode('[mycred_leaderboard number="10"]'); ?>
-    </div>
-</form>
-<script type="text/javascript">
-
-</script>
+<?php _e('my rank', 'woocommerce'); ?>
+<strong><?php echo do_shortcode('[mycred_my_ranking user_id="' . $_GET['uid'] . '"]'); ?></strong>
+<table>
+    <thead>
+    <th style="text-align:center;" colspan="3"><?php _e('ranking list', 'woocommerce'); ?></th>
+    </thead>
+    <thead>
+    <th style="text-align:center;"><?php _e('ranking', 'woocommerce'); ?></th>
+    <th style="text-align:center;"><?php _e('Account username', 'woocommerce'); ?></th>
+    <th style="text-align:center;"><?php _e('points', 'woocommerce'); ?></th>
+    </thead>
+    <?php echo do_shortcode('[mycred_leaderboard number="10"]<tr><td style="text-align:center;">%ranking%</td> <td style="text-align:center;">%display_name%</td><td style="text-align:center;">%cred_f%</td></tr>[/mycred_leaderboard]'); ?>
+</table>
