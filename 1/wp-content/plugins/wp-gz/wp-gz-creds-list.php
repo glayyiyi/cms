7575b2509d356293ae0e645656993546c3d2cab1
MOON <675448353@qq.com>
2014-04-04 22:44:19 +0800

    删除注释

--- 1/wp-content/plugins/wp-gz/wp-gz-creds-list.php
+++ 1/wp-content/plugins/wp-gz/wp-gz-creds-list.php
@@ -42,17 +42,19 @@ function cred_list_adm_html_page()
         <?php //wp_nonce_field('update-options'); ?>
         <h3>积分列表</h3>
 
-        <form method="get" action="" onsubmit="formSubmit()">
+        <form method="get" action="">
             <label for="mobile">手机号</label>
             <input type="hidden" name="page" value="<?php echo $_GET['page'] ;?>"/>
             <input type="text" id="mobile" name="mobile" value="<?php echo $_GET['mobile'] ;?>" placeholder="请输入手机号"/>
             起始时间<input class="Wdate" name="from_date" id="from_date" value="<?php echo $_GET['from_date'] ;?>" type="text" onfocus="WdatePicker({dateFmt: 'yyyy-MM-dd HH:mm:ss', maxDate: '#F{$dp.$D(\'end_date\')}'})">
             截止时间<input class="Wdate" name="end_date" id="end_date" value="<?php echo $_GET['end_date'] ;?>" type="text" onfocus="WdatePicker({dateFmt: 'yyyy-MM-dd HH:mm:ss', minDate: '#F{$dp.$D(\'from_date\')}'})">
             <input type="submit" value="查看">
-
+            </form>
             <?php $cred_page = new Cred_page();
+            $cred_page -> query_gz_cred();
             echo $cred_page->page_navigation();?>
-
+        <form method="get" action="" onsubmit="formSubmit()">
+            <input type="submit" value="兑换">
             <table>
                 <thead>
                 <th style="text-align:center;width:100px;">用户ID</th>
@@ -69,7 +71,6 @@ function cred_list_adm_html_page()
                         update_user_meta($item, 'take_away', 1);
                     }
                 }
-                $cred_page -> query_gz_cred();
 
                 echo $cred_page -> gz_cred_display();
                 ?>
